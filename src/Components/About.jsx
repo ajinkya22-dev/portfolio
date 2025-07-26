01802771dd0d7d0e5362972e91ab1931e7491c7b
ajinkya22-dev <ajinkyapathak999@gmail.com>
2025-07-26 19:21:22 +0530

    content change

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -15,11 +15,10 @@ function About() {
                     <h2 className="text-4xl font-bold mb-4 text-center">About Me</h2>
                     <div className="max-w-2xl mx-auto text-lg text-justify leading-relaxed">
                         <p>
-                            👋 I'm <strong>Ajinkya Pathak</strong>, an Information Technology student at PICT with a solid foundation in cybersecurity and full-stack development. I specialize in building secure, scalable products using technologies like React and Node.js, while also exploring Blue Team defense strategies and real-world threat analysis to strengthen digital infrastructures.
+                            I'm <strong>Ajinkya Pathak</strong>, an Information Technology student at PICT with a solid foundation in cybersecurity and full-stack development. I specialize in building secure, scalable products using technologies like React and Node.js, while also exploring Blue Team defense strategies and real-world threat analysis to strengthen digital infrastructures.
                         </p>
-                        <p className="mt-6">
-                            🛡️ As the creator of <strong>TechVerse</strong>, I regularly publish insights on cybersecurity, blockchain, and emerging technologies. 💼 I'm passionate about contributing to meaningful projects—whether as a Software Development Engineer or a Cybersecurity Analyst—bringing a unique combination of development expertise and a security-first mindset.
-                        </p>
+                        <p className="mt-6">As the creator of <strong>TechVerse</strong>, I regularly publish insights on cybersecurity, blockchain, and emerging technologies. I’m passionate about open-source contribution and actively explore opportunities as a <strong>Full Stack Developer</strong> or <strong>Cybersecurity Analyst</strong>, bringing a unique blend of development expertise and a security-first mindset.</p>
+
                     </div>
                 </motion.div>
 
